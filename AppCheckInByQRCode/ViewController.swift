81e10d8bd3c7bce0328e576fe1da635182d6b8a3
htkien <htkien2511@gmail.com>
2020-06-26 09:30:49 +0700

    Initial Commit

---
+++ AppCheckInByQRCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCheckInByQRCode
+//
+//  Created by Hoang Trong Kien on 6/26/20.
+//  Copyright © 2020 Hoang Trong Kien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
