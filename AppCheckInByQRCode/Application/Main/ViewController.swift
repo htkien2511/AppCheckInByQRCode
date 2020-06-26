5048d29ae06fb457fcbc8af34350a1e1544676d6
htkien <htkien2511@gmail.com>
2020-06-26 16:40:38 +0700

    Add function in login screen

--- AppCheckInByQRCode/Application/Main/ViewController.swift
+++ AppCheckInByQRCode/Application/Main/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   // MARK: - Outlets
   @IBOutlet weak var containerLoginView: UIView!
   @IBOutlet weak var loginButton: UIButton!
@@ -17,14 +17,65 @@ class ViewController: UIViewController {
   @IBOutlet weak var emailLabel: UILabel!
   @IBOutlet weak var passwordLabel: UILabel!
   @IBOutlet weak var errorLabel: UILabel!
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
   
   // MARK: -
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    emailTextField.delegate = self
+    passwordTextField.delegate = self
     setUpElements()
+    setUpViewWhenKeyboardAppear()
+    setUpImageViewInsideTextField()
   }
-
+  
+  func setUpImageViewInsideTextField() {
+    changeImageView(textField: emailTextField, nameImage: "email")
+    changeImageView(textField: passwordTextField, nameImage: "password")
+  }
+  
+  func changeImageView(textField: UITextField, nameImage: String) {
+    if let myImage = UIImage(named: nameImage){
+        textField.withImage(direction: .Left, image: myImage, colorSeparator: UIColor.blue, colorBorder: UIColor.black)
+    }
+  }
+  
+  func setUpViewWhenKeyboardAppear() {
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                           name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                           name: UIResponder.keyboardWillHideNotification, object: nil)
+  }
+  
+  @objc func keyboardWillShow(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+      if self.view.frame.origin.y == 0 {
+        self.view.frame.origin.y -= keyboardSize.height
+      }
+    }
+    
+    //
+    let viewIsTap = UITapGestureRecognizer(target: self, action: #selector(viewIsTapped))
+    view.addGestureRecognizer(viewIsTap)
+  }
+  
+  @objc func keyboardWillHide(notification: NSNotification) {
+    if self.view.frame.origin.y != 0 {
+      self.view.frame.origin.y = 0
+    }
+    
+    //
+    view.gestureRecognizers?.removeAll(where: { (gestureRecognizer) -> Bool in
+      return gestureRecognizer is UITapGestureRecognizer
+    })
+  }
+  
+  @objc func viewIsTapped() {
+    view.endEditing(true)
+  }
+  
   func setUpElements() {    
     containerLoginView.shadow()
     containerLoginView.corner(radius: 15)
@@ -39,3 +90,18 @@ class ViewController: UIViewController {
   }
 }
 
+// MARK: - Text Field Delegate
+extension ViewController: UITextFieldDelegate {
+  // move into password textfield
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    let nextTag = textField.tag + 1
+    
+    if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+      nextResponder.becomeFirstResponder()
+    } else {
+      textField.resignFirstResponder()
+      print("Pass data to database")
+    }
+    return true
+  }
+}
