ea053c7476ca9b2e8b8b02fe0d62f93ae05ec275
htkien <htkien2511@gmail.com>
2020-07-02 08:37:16 +0700

    Fix bug

--- AppCheckInByQRCode/Application/SignUp/SignUpViewController.swift
+++ AppCheckInByQRCode/Application/SignUp/SignUpViewController.swift
@@ -36,7 +36,7 @@ class SignUpViewController: UIViewController {
   @objc func keyboardWillShow(notification: NSNotification) {
     if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
       if inputTextField[0].isFirstResponder || inputTextField[1].isFirstResponder {
-        print("a")
+        
       }
       else if self.view.frame.origin.y == 0 {
         self.view.frame.origin.y -= keyboardSize.height
