1f7e290876585329ccca03e2cd92f6fabae3c608
htkien <htkien2511@gmail.com>
2020-07-06 16:06:00 +0700

    rename

--- AppCheckInByQRCode/Application/ManagerTabBarViewController/ManagerTabBarViewController.swift
+++ AppCheckInByQRCode/Application/ManagerTabBarViewController/ManagerTabBarViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeTabViewController: UITabBarController {
+class ManagerTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
